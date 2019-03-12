03106023a567ad9ce873fce493b8ed2241af0bc9
Luke Storry <luke@lukestorry.co.uk>
2019-03-12 13:31:12 +0000

    updated setupscript to not use lab machines, and added readme

--- openday-setup.py
+++ openday-setup.py
@@ -1,16 +1,23 @@
 #!/usr/local/bin/python
+
+"""
+This file should be run on snowy with a passwords.csv file in the same folder.
+If you are updating the version of processing, be sure to update the number in all four places.
+"""
+
 import csv
 import pexpect
 
 print("Downloading Processing's .tgz to copy")
-downloader = pexpect.spawn('wget -nc -O processing.tgz http://download.processing.org/processing-3.4-linux64.tgz')
+downloader = pexpect.spawn('wget -nc http://download.processing.org/processing-3.5.3-linux64.tgz')
 downloader.expect(pexpect.EOF)
+print downloader.read()
 
 print("Creating .bashprofile_temp to copy")
 with open('temp_bash_profile', 'w') as bashprofile:
     bashprofile.write('firefox http://lukestorry.com/code-a-clock & ')
-    bashprofile.write('tar -xzf ~/processing.tgz;\n')
-    bashprofile.write('~/processing-3.4/processing &')
+    bashprofile.write('tar -xzf ~/processing-3.5.3-linux64.tgz;\n')
+    bashprofile.write('~/processing-3.5.3/processing &')
 
 with open('passwords.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
@@ -18,7 +25,7 @@ with open('passwords.csv', 'r') as csvfile:
 	print
         print "SCPing files to", username, password
 
-        scp_bash = pexpect.spawn("scp temp_bash_profile %s@it025960.users.bris.ac.uk:./.bash_profile" % username)
+        scp_bash = pexpect.spawn("scp temp_bash_profile %s@snowy.cs.bris.ac.uk:./.bash_profile" % username)
         needs_confimation = scp_bash.expect(["password", "yes"])
         if needs_confimation:
             scp_bash.sendline("yes")
@@ -26,7 +33,7 @@ with open('passwords.csv', 'r') as csvfile:
         scp_bash.sendline(password)
 	print scp_bash.read(),
 
-        scp_processing = pexpect.spawn("scp processing.tgz %s@it025960.users.bris.ac.uk:." % username)
+        scp_processing = pexpect.spawn("scp processing-3.5.3-linux64.tgz %s@snowy.cs.bris.ac.uk:." % username)
         scp_processing.expect("password", timeout=5)
         scp_processing.sendline(password)
 	print scp_processing.read()
